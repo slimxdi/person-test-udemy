c4b02019ac62e71b7f98d2ffd28e7c700449761f
Daniel Hsieh <daniel.x.hsieh@gmail.com>
2020-03-02 11:29:09 -0500

    added initial state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import Person from './Person/Person'
 import './App.css';
 
 class App extends Component {
+  state = {
+    persons: [
+      { personname: 'Archie', age: 30},
+      { personname: 'Barker', age: 31},
+      { personname: 'Cedric', age: 32}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-        <Person personname="Archie" age="30"></Person>
-        <Person personname="Barker" age="31">I like turtles.</Person>
+        <Person personname={this.state.persons[0].personname} age="30"></Person>
+        <Person personname={this.state.persons[1].personname} age="31">I like turtles.</Person>
+        <Person personname={this.state.persons[2].personname} age="32">I like broccoli corn chowder.</Person>
       </div>
     )
   }
