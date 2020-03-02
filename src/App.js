d7fa34021410144fddda30e20dd7d7e608df8933
Daniel Hsieh <daniel.x.hsieh@gmail.com>
2020-03-02 14:13:19 -0500

    converted App to { useState } hook component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import Person from './Person/Person'
 import './App.css';
 
-class App extends Component {
-  state = {
-    persons: [
-      { personname: 'Archie', age: 30},
-      { personname: 'Barker', age: 31},
-      { personname: 'Cedric', age: 32}
-    ]
-  }
-
-  switchNameHandler = () => {
-    this.setState({
+const App = props => {
+  const [personState, setPersonState] = useState(
+    {
       persons: [
-        { personname: 'PsuedoArchie', age: 60},
-        { personname: 'PsuedoBarker', age: 61},
-        { personname: 'PsuedoCedric', age: 62}
+        { personname: 'Archie', age: 30},
+        { personname: 'Barker', age: 31},
+        { personname: 'Cedric', age: 32}
       ]
-    })
-  }
+    }
+  );
 
-  render() {
-    return (
-      <div className="App">
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person personname={this.state.persons[0].personname} age={this.state.persons[0].age}></Person>
-        <Person personname={this.state.persons[1].personname} age={this.state.persons[1].age}>I like turtles.</Person>
-        <Person personname={this.state.persons[2].personname} age={this.state.persons[2].age}>I like broccoli corn chowder.</Person>
-      </div>
+  const switchNameHandler = () => {
+    setPersonState(
+      {
+      persons: [
+          { personname: 'PsuedoArchie', age: 60},
+          { personname: 'PsuedoBarker', age: 61},
+          { personname: 'PsuedoCedric', age: 62}
+        ]
+      }
     )
-  }
-}
+  };
+
+  return (
+    <div className="App">
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person personname={personState.persons[0].personname} age={personState.persons[0].age}></Person>
+      <Person personname={personState.persons[1].personname} age={personState.persons[1].age}>I like turtles.</Person>
+      <Person personname={personState.persons[2].personname} age={personState.persons[2].age}>I like broccoli corn chowder.</Person>
+    </div>
+  );
+};
 
 export default App;
+
+// state = {
+//   persons: [
+//     { personname: 'Archie', age: 30},
+//     { personname: 'Barker', age: 31},
+//     { personname: 'Cedric', age: 32}
+//   ]
+// }
+
